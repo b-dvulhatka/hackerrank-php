bc5d3144979d065a1124b94adecbbd0b91801dea
b-dvulhatka <dvulhatk4@gmail.com>
2020-01-23 17:09:25 -0300

    Problem Solving
    45 updates

---
+++ Problem Solving/Electronics Shop.php
@@ -0,0 +1,51 @@
+<?php
+
+
+/*
+ * Complete the getMoneySpent function below.
+ */
+function getMoneySpent($keyboards, $drives, $b) {
+    rsort($keyboards);
+    rsort($drives);
+
+    $allows = [];
+    foreach ($keyboards as $keyboard) {
+        foreach ($drives as $drive) {
+            if ($keyboard + $drive <= $b) {
+                $allows[] = $keyboard + $drive;
+            }
+        }
+    }
+
+    return !empty($allows) ? max($allows) : -1;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%[^\n]", $bnm_temp);
+$bnm = explode(' ', $bnm_temp);
+
+$b = intval($bnm[0]);
+
+$n = intval($bnm[1]);
+
+$m = intval($bnm[2]);
+
+fscanf($stdin, "%[^\n]", $keyboards_temp);
+
+$keyboards = array_map('intval', preg_split('/ /', $keyboards_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+fscanf($stdin, "%[^\n]", $drives_temp);
+
+$drives = array_map('intval', preg_split('/ /', $drives_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+
+
+$moneySpent = getMoneySpent($keyboards, $drives, $b);
+
+fwrite($fptr, $moneySpent . "\n");
+
+fclose($stdin);
+fclose($fptr);
